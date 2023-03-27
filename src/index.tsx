cb62db07841b56e703914fa8fac61d3800b60747
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-03-28 00:35:48 +0300

    change Hash on BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./bll/store";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   // <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
   // </React.StrictMode>
 );
 
