25602d1b739b101f7c06a0a087aca72de8a378a7
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-10-26 20:00:21 +0300

    FIX: displaying a navbar for an inactive account

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import s from './Main.module.scss';
 import {Navbar} from "./Navbar/Navbar";
 import {useSelector} from "react-redux";
-import {getIsAuth} from "../../bll/auth.selector";
+import {getIsActivated, getIsAuth} from "../../bll/auth.selector";
 import {AppRoutes} from "../../routes/Routes";
 
 export const Main = () => {
 
     const isAuth = useSelector(getIsAuth);
+    const isActivated = useSelector(getIsActivated);
 
     return (
         <div className={s.mainWrapper}>
             <div className='container'>
-                {isAuth && <Navbar/>}
+                {isAuth && isActivated && <Navbar/>}
                 <div className={s.content}>
                     <AppRoutes/>
                 </div>
